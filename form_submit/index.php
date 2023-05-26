7ce890d4443eacbc767a737778380dfb457955e0
michaelpitop <michaelpitop@outlook.com>
2023-05-26 22:56:20 +0200

    now the app stores the name and email in RAM

--- form_submit/index.php
+++ form_submit/index.php
@@ -1,4 +1,7 @@
 <?php
+// Start or resume the session
+session_start();
+
 // Process form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve form data
@@ -9,25 +12,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($name) || empty($email)) {
         $error = "Please fill in all fields";
     } else {
-        // Perform some backend logic or database operations here
+        // Store the submitted data in session variables
+        $_SESSION['users'][] = array('name' => $name, 'email' => $email);
 
         // Display success message
         $message = "Form submitted successfully";
     }
 }
+
+// Retrieve stored data from session
+$storedData = isset($_SESSION['users']) ? $_SESSION['users'] : array();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Simple PHP Application</title>
+    <title>Submit Form and Showcase Data</title>
 </head>
 <body>
-    <h1>Simple PHP Application</h1>
-
-    <?php if (isset($error)) { ?>
-        <p style="color: red;"><?php echo $error; ?></p>
-    <?php } ?>
+    <h1>Submit Form and Showcase Data</h1>
 
     <?php if (isset($message)) { ?>
         <p style="color: green;"><?php echo $message; ?></p>
@@ -42,5 +45,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <input type="submit" value="Submit">
     </form>
+
+    <h2>Stored Data:</h2>
+    <?php if (!empty($storedData)) { ?>
+        <ul>
+            <?php foreach ($storedData as $userData) { ?>
+                <li><?php echo $userData['name'] . ' - ' . $userData['email']; ?></li>
+            <?php } ?>
+        </ul>
+    <?php } else { ?>
+        <p>No data available</p>
+    <?php } ?>
 </body>
 </html>
