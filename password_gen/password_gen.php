4180740bde4cb290b5ada9b9ba075dd2c581e0d7
michaelpitop <michaelpitop@outlook.com>
2023-05-28 15:04:09 +0200

    random password generator with user input on password length

---
+++ password_gen/password_gen.php
@@ -0,0 +1,19 @@
+<?php
+function generatePassword($length = 8) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $password .= $characters[$index];
+    }
+
+    return $password;
+}
+
+echo "Enter password length: ";
+$length = intval(trim(fgets(STDIN)));
+
+$password = generatePassword($length);
+echo "Generated password: $password\n";
+?>
