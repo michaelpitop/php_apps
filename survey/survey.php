b15e65934c73c3afbe504cb541454842341cb1e9
michaelpitop <michaelpitop@outlook.com>
2023-06-13 21:01:57 +0200

    changed the option selection to numbers instead of letters, makes it easier to chooce

--- survey/survey.php
+++ survey/survey.php
@@ -1,5 +1,4 @@
 <?php
-
 $questions = [
     'Q1. What is your favorite color?',
     'Q2. What is your favorite animal?',
@@ -7,18 +6,17 @@ $questions = [
 ];
 
 $options = [
-    'Q1' => ['A' => 'Red', 'B' => 'Blue', 'C' => 'Green'],
-    'Q2' => ['A' => 'Dog', 'B' => 'Cat', 'C' => 'Bird'],
-    'Q3' => ['A' => 'Pizza', 'B' => 'Burger', 'C' => 'Sushi'],
+    'Q1' => ['1' => 'Red', '2' => 'Blue', '3' => 'Green'],
+    'Q2' => ['1' => 'Dog', '2' => 'Cat', '3' => 'Bird'],
+    'Q3' => ['1' => 'Pizza', '2' => 'Burger', '3' => 'Sushi'],
 ];
 
 $responses = [];
 
-
 foreach ($questions as $key => $question) {
     echo $question . PHP_EOL;
-    $questionKey = 'Q' . ($key + 1); 
-    $questionOptions = $options[$questionKey] ?? null; 
+    $questionKey = 'Q' . ($key + 1);
+    $questionOptions = $options[$questionKey] ?? null;
     if (!$questionOptions) {
         echo "Options for question $questionKey not found." . PHP_EOL;
         continue;
@@ -35,13 +33,11 @@ foreach ($questions as $key => $question) {
     $responses[] = $questionOptions[$response];
 }
 
-
 $statistics = [];
 foreach ($options as $questionKey => $questionOptions) {
-    $questionStats = array_count_values($responses)[$questionOptions['A']] ?? 0;
+    $questionStats = array_count_values($responses)[$questionOptions['1']] ?? 0;
     $statistics[$questionKey] = $questionStats;
 }
-
 
 echo PHP_EOL . 'Survey Statistics:' . PHP_EOL;
 foreach ($statistics as $questionKey => $questionStats) {
